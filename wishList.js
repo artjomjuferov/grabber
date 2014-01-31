f7d0042da020383f62c0cb0f4453a3928f783ea1
Artjom Juferov <derevjaginti@gmail.com>
2014-01-31 22:47:15 +0300

    Revert "some changes"
    This reverts commit 6bc4804e145060dbabb623552a508f273f55fa8f.

--- wishList.js
+++ wishList.js
@@ -1,3 +1,47 @@
+  
+function findChildImg(url){
+  if (url != ""){
+    var a = $("a[href='"+url+"']").find('img').attr('src');
+    return a;
+  }
+}
+
+function isImg(url){
+  if (url == null){
+    return 0;
+  }
+  var patt1 = /.png$|.jpg$/i;
+  var result = url.match(patt1);
+  if(result != null){
+    return 1;
+  }else{
+    return 0;
+  }
+}
+
+function noopHandler(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+}
+
+function drop(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    $('#wlDrag').empty();
+    var imageUrl = evt.dataTransfer.getData("url");
+    if (isImg(imageUrl) == 0){
+      imageUrl = findChildImg(imageUrl);
+      if (isImg(imageUrl) == 0){
+        return false;
+      }    
+    }
+    var dragImg = document.createElement("img");
+    dragImg.setAttribute('id',"wlDragImg");
+    dragImg.setAttribute('src',imageUrl);
+    document.getElementById('wlDrag').appendChild(dragImg);
+}
+
 window.createDiv = function(){
   var jqueryScript1 = document.createElement("script");
 
@@ -7,10 +51,7 @@ window.createDiv = function(){
   var jqueryScript2 = document.createElement("script");
   jqueryScript2.type = "text/javascript";
   jqueryScript2.src = " http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js";
-
-  document.getElementsByTagName('head')[0].appendChild(jqueryScript1);
-  document.getElementsByTagName('head')[0].appendChild(jqueryScript2);
-
+  
   var mainDiv = document.createElement("div");
   mainDiv.setAttribute('id',"wlMain");
   mainDiv.style.zIndex = "1000";
@@ -21,25 +62,18 @@ window.createDiv = function(){
   var textDiv = document.createElement("div");
   textDiv.setAttribute('id',"wlText");
 
-  var i1 = document.createElement("input"); //input element, text
-  i1.setAttribute('type',"text");
-  i1.setAttribute('name',"fileName");
-  i1.setAttribute('id',"wlInput");
-
-  var i2 = document.createElement("input"); //input element, text
-  i2.setAttribute('type',"text");
-  i2.setAttribute('name',"desription");
-  i2.setAttribute('id',"wlInputDesc");
+  var i = document.createElement("input"); //input element, text
+  i.setAttribute('type',"text");
+  i.setAttribute('name',"username");
+  i.setAttribute('id',"wlInput");
 
   var s = document.createElement("input"); //input element, Submit button
   s.setAttribute('type',"button");
   s.setAttribute('value',"Send");
   s.setAttribute('id',"wlSend");
 
-  textDiv.appendChild(i1);
-  textDiv.appendChild(i2);
+  textDiv.appendChild(i);
   textDiv.appendChild(s);
-
 
   dragDiv = document.createElement("div");
   dragDiv.setAttribute('id',"wlDrag");
@@ -62,57 +96,13 @@ window.createDiv = function(){
   mainDiv.appendChild(textDiv);
   mainDiv.appendChild(dragDiv);
   mainDiv.appendChild(allImg);
-  
+
   document.getElementsByTagName('body')[0].appendChild(mainDiv);
+  document.getElementsByTagName('body')[0].appendChild(jqueryScript1);
+  document.getElementsByTagName('body')[0].appendChild(jqueryScript2);
+
 };
 
-function findChildImg(url){
-  if (url != ""){
-    var a = $("a[href='"+url+"']").find('img').attr('src');
-    return a;
-  }
-}
-
-function isImg(url){
-  if (url == null){
-    return 0;
-  }
-  var patt1 = /.png$|.jpg$|.gif$/i;
-  var result = url.match(patt1);
-  if(result != null){
-    return 1;
-  }else{
-    return 0;
-  }
-}
-
-function noopHandler(evt) {
-    evt.stopPropagation();
-    evt.preventDefault();
-}
-
-function drop(evt) {
-    evt.stopPropagation();
-    evt.preventDefault();
-
-    $('#wlDrag').empty();
-    var imageUrl = evt.dataTransfer.getData("url");
-    imageUrl = imageUrl.split('?')[0];
-    if (isImg(imageUrl) == 0){
-      imageUrl = findChildImg(imageUrl);
-      if (imageUrl == null){
-        return false;
-      }
-      imageUrl = imageUrl.split('?')[0];
-      if (isImg(imageUrl) == 0){
-        return false;
-      }    
-    }
-    var dragImg = document.createElement("img");
-    dragImg.setAttribute('id',"wlDragImg");
-    dragImg.setAttribute('src',imageUrl);
-    document.getElementById('wlDrag').appendChild(dragImg);
-}
 
 window.createDiv();
 
@@ -146,10 +136,8 @@ $(function() {
     $('#wlShowImgs').empty();
     $('#wlShowImgs').remove();
     $('#wlDrag').empty();
-    var tmp1 = $("#wlInput").val();
-    var tmp2 = $("#wlInputDesc").val();
-    var tmp3 = $("#wlDragImg").attr("src");
-    
-    alert(tmp1 + ' ' + tmp2 + ' ' + tmp3);
+    var tmp = $("#wlInput").val();
+    alert(tmp);
   });
 });
+
