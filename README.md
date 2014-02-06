31ad5212efae7536e6a30aec8946854b49809059
Artjom Juferov <derevjaginti@gmail.com>
2014-02-06 18:41:43 +0300

    jquery make better

--- README.md
+++ README.md
@@ -2,4 +2,5 @@ clipper
 =======
 
 create bookmark with url:
-"javascript:var s = document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='http://path_to_file/wishList.js';void(0);"
+
+javascript:var v = document.createElement('script');v.type='text/javascript';document.body.appendChild(v);v.src='http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js';var p = document.createElement('script');p.type='text/javascript';document.body.appendChild(p);p.src='http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js';var s = document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='http://127.0.0.1/wishList.js';void(0);
--- wishList.js
+++ wishList.js
@@ -1,16 +1,7 @@
+jQuery( document ).ready(function() {
+   
 
 window.createDiv = function(){
-   var jqueryScript1 = document.createElement("script");
-
-  jqueryScript1.type = "text/javascript";
-  jqueryScript1.src = "http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js";
-  
-  var jqueryScript2 = document.createElement("script");
-  jqueryScript2.type = "text/javascript";
-  jqueryScript2.src = " http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js";
-
-  document.getElementsByTagName('body')[0].appendChild(jqueryScript1);
-  document.getElementsByTagName('body')[0].appendChild(jqueryScript2);
 
   var mainDiv = document.createElement("div");
   mainDiv.setAttribute('id',"wlMain");
@@ -154,4 +145,6 @@ $jq('#wlSend').click(function() {
   var tmp1 = $jq("#wlInput").val();
   var tmp2 = $jq("#wlDragImg").src;
   alert(tmp1+' '+tmp2);
+});
+
 });
